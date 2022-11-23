78d1db4363d4003ad09eb2a0130e8fce533c2c42
VolpinAndrea <andvolpin19@gmail.com>
2022-11-23 09:58:31 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+String[] mesi = { "gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre" };
+for(int i = 0; i < mesi.Length; i++)  //><
+{
+    Console.WriteLine((i+1)+ " "+mesi[i]);
+}           
