92563cb9d3b423d7e3d3de57798a9cdd4cf6df4a
VolpinAndrea <andvolpin19@gmail.com>
2022-11-23 10:02:45 +0100

    1b

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Snack 1
 String[] mesi = { "gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre" };
 for(int i = 0; i < mesi.Length; i++)  //><
 {
     Console.WriteLine((i+1)+ " "+mesi[i]);
-}           
+}
+//Snack 1b
+mesi[6] = "Luglio-Estate";
+foreach (string mese in mesi)
+{
+    Console.WriteLine(mese);
+}
